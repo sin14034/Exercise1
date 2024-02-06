09f10c923eefed8db17dbe31110f380751c198c8
dalwindersingh <sin14034@sheridancollege.ca>
2024-02-06 12:03:41 -0500

    Author is Changed

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -5,8 +5,8 @@ package exercise1;
  * It then searches the array of cards for the match to the user's card. 
  * To be used as starting code in Exercise
  *
- * @author dancye
- * @author Paul Bonenfant Jan 25, 2022 
+ * @author Dalwinder singh
+ * @author Dalwinder singh 06, 2024 
  */
 public class CardTrick {
     
